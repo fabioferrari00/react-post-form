3cceb49e847b277d648511a39a881def6be0381b
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-06 14:15:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Hello world</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
