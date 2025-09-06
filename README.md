ac9889682174565cfeb2109f413381008323e513
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-06 14:24:59 +0200

    [feat] add form and input camps

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ title (string) - Il titolo del post
 body (string) - Il testo del post
 public (boolean) - Se il post deve essere pubblico (true) o una bozza (false)
 
-L’endpoint a cui effettuare la chiamata POST è il seguente: https://**67c5b4f3351c081993fb1ab6**.mockapi.io**/api**/posts 
+L’endpoint a cui effettuare la chiamata POST è il seguente: https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts 
 
 Questo endpoint, in caso di successo, vi restituisce i dati inviati. Stampateli in console per verificare di essere riusciti ad inviare correttamente tutto!
--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,38 @@ function App() {
           <div className="col-12">
             <h1>Hello world</h1>
           </div>
+          <div className="col-12">
+            <form>
+              <div className="row">
+                <div className="col-12 col-md-4">
+                  <label className="form-label">Autore</label>
+                  <input type="text"
+                    className="form-control"
+                    placeholder="Autore" />
+                </div>
+                <div className="col-12 col-md-4">
+                  <label className="form-label">Titolo</label>
+                  <input type="text"
+                    className="form-control"
+                    placeholder="Titolo" />
+                </div>
+                <div className="col-12 col-md-4">
+                  <label className="form-label">Pubblico</label>
+                  <div>
+                    <input type="checkbox"
+                      className="form-check-input" />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Testo</label>
+                  <textarea name="" id="" className="form-control" placeholder="Descrizione"></textarea>
+                </div>
+                <div className="col-12">
+                  <button className="btn btn-success">Salva</button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
