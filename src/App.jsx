d62fbe92525e4aa7b6ca08ee5e996678cf0ce78d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-06 15:05:35 +0200

    [feat] add handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+  })
+
+  const handleChange = (e) => {
+    const { value, name, type, checked } = e.target;
+    const newFormData = {
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value
+    };
+    setFormData(newFormData)
+  }
+
 
   return (
     <>
@@ -11,29 +29,38 @@ function App() {
           </div>
           <div className="col-12">
             <form>
-              <div className="row">
+              <div className="row gy-2">
                 <div className="col-12 col-md-4">
                   <label className="form-label">Autore</label>
                   <input type="text"
+                    name="author"
+                    value={formData.author}
                     className="form-control"
-                    placeholder="Autore" />
+                    placeholder="Autore"
+                    onChange={handleChange} />
                 </div>
                 <div className="col-12 col-md-4">
                   <label className="form-label">Titolo</label>
                   <input type="text"
+                    name="title"
+                    value={formData.title}
                     className="form-control"
-                    placeholder="Titolo" />
+                    placeholder="Titolo"
+                    onChange={handleChange} />
                 </div>
                 <div className="col-12 col-md-4">
                   <label className="form-label">Pubblico</label>
                   <div>
                     <input type="checkbox"
-                      className="form-check-input" />
+                      name="public"
+                      checked={formData.public}
+                      className="form-check-input"
+                      onChange={handleChange} />
                   </div>
                 </div>
                 <div className="col-12">
                   <label className="form-label">Testo</label>
-                  <textarea name="" id="" className="form-control" placeholder="Descrizione"></textarea>
+                  <textarea name="body" id="body" value={formData.body} className="form-control" placeholder="Descrizione" onChange={handleChange}></textarea>
                 </div>
                 <div className="col-12">
                   <button className="btn btn-success">Salva</button>
